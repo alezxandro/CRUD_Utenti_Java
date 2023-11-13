96d6d7b6324c4cc23b8c3f02751d8e879aba5b5e
alezxandro <gheojan.alessandro@outlook.com>
2023-11-13 18:15:28 +0100

    main

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,5 +1,31 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.*;
+import controller.*;
+
 public class Test {
-    
+    public static void main(String[] args) {
+        Utente utente = new Utente("null", "null");
+
+        List<Utente> utenti = new ArrayList<>();
+        utenti.add(utente);
+
+        AuthenticationController authenticationController = new AuthenticationController(utenti);
+
+        authenticationController.login("null", "null");
+
+        authenticationController.creaUtente("null", "null");
+
+        authenticationController.rimuoviUtente(utente);
+
+        UserController userController = new UserController(utente);
+
+        userController.visualizzaProfilo();
+
+        userController.aggiornaProfilo();
+
+    }
 }
